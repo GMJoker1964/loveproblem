1aad8fc96665d3e768b092f0d304ec6774eef369
GMJoker1964 <38500531+GMJoker1964@users.noreply.github.com>
2019-01-18 14:51:14 +0700

    Add files via upload

---
+++ loveme2/randon.js
@@ -0,0 +1,10 @@
+function yesButton() {
+    alert("Aww! I love u so.");
+}
+function noHover() {
+    var x = Math.random() * window.innerHeight;
+    var y = Math.random() * window.innerWidth;
+
+    document.getElementById("no").style.left = x + "px";
+    document.getElementById("no").style.top = y + "px";
+}
